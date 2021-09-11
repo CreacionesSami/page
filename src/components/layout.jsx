63d046fd22bfb03434c456e13fbc0e21e7aa9547
sebastian2000pk <juochoas@unal.edu.co>
2021-09-11 09:23:14 -0500

    firts commit

---
+++ src/components/layout.jsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+import "./layout.css"
+
+import NavBar from "./nav/NavBar"
+
+const Layout = ({ children }) => {
+
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+
+export default Layout
---
+++ src/components/nav/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './navBar.css';
+
+
+const NavBar = () => {
+  return (
+    <nav className="nav__container">
+      <div className="hamburguer-button">
+        <div className="hamburguer">
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;

5b8e1bb34de5d98e3f4f9617c7fb7eeb5756a7d2
sebastian2000pk <juochoas@unal.edu.co>
2021-09-12 02:12:59 -0500

    Updates

---
+++ component---src-pages-index-js-50f700d8e7f24ca2e60d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{7704:function(t,e,a){a.r(e);var u=a(7294),r=(a(5444),a(5750));a(3751);e.default=function(){return u.createElement(r.Z,null)}}}]);
+//# sourceMappingURL=component---src-pages-index-js-50f700d8e7f24ca2e60d.js.map
